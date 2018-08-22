3c96666d27880f954707a91e9ef47293d64798db
dylee <leeduyoung20@naver.com>
2018-08-22 17:03:37 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Platform, Text, View } from 'react-native';
+import styles from './styles';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# react-native-ts-seed
+# -React-Native-Tab-Ts
